c44433d21fe5334ce6bc1c4205125c5222f68fab
DavidSeven <david07seven@gmail.com>
2016-03-27 17:48:46 -0500

    added: queues and list

--- Unicor OS simulator/src/models/Management.java
+++ Unicor OS simulator/src/models/Management.java
@@ -1,7 +1,12 @@
 package models;
 
+import java.util.List;
+import java.util.Queue;
+
 public class Management 
 {
 	private short velocity; //tiempo de espera para cada thread en milisegundos para copiar cada caracter
-		
+	private Queue readyQueue; //cola de procesos listos
+	private Queue stopQueue; //cola de procesos detenidos
+	private List listOfCompleted; //lista de procesos terminados
 }
